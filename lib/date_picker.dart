6c6c940c75fc3f7a3a66e12748d5cea92b430ac5
Olga Kalitova <olga.kalitova@gmail.com>
2020-06-06 00:27:34 +0300

    Separate DatePicker to a separate file and class

---
+++ lib/date_picker.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DatePicker extends StatefulWidget {
+  final Function onDateChanged;
+
+  const DatePicker({Key key, this.onDateChanged}) : super(key: key);
+
+  @override DatePickerState createState() => DatePickerState();
+}
+
+class DatePickerState extends State<DatePicker> {
+  DateTime _dateTime = new DateTime.now();
+  static const one_day = const Duration(days: 1);
+  var formatter = new DateFormat('dd/MM/yyyy');
+
+  void _decrementDate() {
+    setState(() {
+      _dateTime = _dateTime.subtract(one_day);
+      widget.onDateChanged(_dateTime);
+    });
+  }
+
+  void _incrementDate() {
+    setState(() {
+      _dateTime = _dateTime.add(one_day);
+      widget.onDateChanged(_dateTime);
+    });
+  }
+
+  @override Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            onPressed: _decrementDate,
+            child: Icon(Icons.chevron_left),
+          ),
+        ),
+        Text(
+          formatter.format(_dateTime),
+          style: Theme.of(context).textTheme.headline5,
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: _incrementDate,
+            child: Icon(Icons.chevron_right),
+          ),
+        ),
+      ],
+    );
+  }
+}
