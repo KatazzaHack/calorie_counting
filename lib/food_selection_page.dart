1c6de48e9168f9bf06dedda3886cab6144a88739
Olga Kalitova <olga.kalitova@gmail.com>
2020-06-06 04:22:40 +0300

    Add month picker to monthly statistics widget

--- lib/food_selection_page.dart
+++ lib/food_selection_page.dart
@@ -28,7 +28,6 @@ class FoodSelectionPage extends StatelessWidget {
               onPressed: null,
             ),
           ],
-
         ),
       ),
     );
--- lib/statistics_page.dart
+++ lib/statistics_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'date_month_picker.dart';
+import 'package:logging/logging.dart';
 
 class MonthlyStatistics extends StatelessWidget {
+  var log = Logger("MonthlyStatistics");
+
+  void _onMonthChanged(int month) {
+    log.fine("_onMonthChanged, now month = " + month.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Monthly Statistics", style: const TextStyle(fontSize: 36)),
+    return Column(
+      children: <Widget> [
+        DateMonthPicker(
+          onMonthChanged: _onMonthChanged,
+        ),
+        Center(
+          child: Text("Monthly Statistics", style: const TextStyle(fontSize: 36)),
+        ),
+      ],
     );
   }
 }
