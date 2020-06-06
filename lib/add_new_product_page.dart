74fcdbe95f3e5a6bd3fb90159ac32980a39cc3ac
Olga Kalitova <olga.kalitova@gmail.com>
2020-06-06 06:59:48 +0300

    Separate adding new meal entities into separate files

---
+++ lib/add_new_product_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AddNewProductPage extends StatelessWidget {
+  @override Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add new product"),
+      ),
+      body: Text("Add new product page"),
+    );
+  }
+}
--- lib/food_selection_page.dart
+++ lib/food_selection_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'add_new_dish_page.dart';
+import 'add_new_product_page.dart';
 import 'add_product.dart';
 
 class FoodSelectionPage extends StatelessWidget {
@@ -11,35 +13,49 @@ class FoodSelectionPage extends StatelessWidget {
       body: Column(
         children: <Widget>[
           AddProductPage(),
-          Expanded(
-            child: Align(
-              alignment: FractionalOffset.bottomCenter,
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: RaisedButton(
-                      child: new Text("Add new dish"),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => AddProductPage()),
-                        );
-                      },
-                    ),
-                  ),
-                  Expanded(
-                    child:
-                    RaisedButton(
-                      child: new Text("Add new product"),
-                      onPressed: null,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          NewMealEntityButtons(),
         ],
       ),
     );
   }
+}
+
+class NewMealEntityButtons extends StatelessWidget {
+  @override Widget build(BuildContext context) {
+    return Expanded(
+      child: Align(
+        alignment: FractionalOffset.bottomCenter,
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: RaisedButton(
+                child: new Text("Add new dish"),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AddNewDishPage()),
+                  );
+                },
+              ),
+            ),
+            Expanded(
+              child:
+              RaisedButton(
+                child: new Text("Add new product"),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => AddNewProductPage()
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
 }
